8d4b4839356e3104e7eaff28710c4c865ec5601e
dwinner <dwinner@inbox.ru>
2016-08-02 12:48:08 +0300

    Rebase the knockout JS sample

---
+++ Knockout-JS/Knockout Interop/Controllers/ProductController.cs
@@ -0,0 +1,37 @@
+﻿using System.Web.Mvc;
+using Mvc4KnockoutCRUD.Models;
+
+namespace Mvc4KnockoutCRUD.Controllers
+{
+   public class ProductController : Controller
+   {
+      private static readonly IProductRepository Repository = new ProductRepository();
+
+      public ActionResult Product()
+      {
+         return View();
+      }
+
+      public JsonResult GetAllProducts()
+      {
+         return Json(Repository.GetAll(), JsonRequestBehavior.AllowGet);
+      }
+
+      public JsonResult AddProduct(Product item)
+      {
+         item = Repository.Add(item);
+         return Json(item, JsonRequestBehavior.AllowGet);
+      }
+
+      public JsonResult EditProduct(int id, Product product)
+      {
+         product.Id = id;
+         return Repository.Update(product) ? Json(Repository.GetAll(), JsonRequestBehavior.AllowGet) : Json(null);
+      }
+
+      public JsonResult DeleteProduct(int id)
+      {
+         return Json(Repository.Delete(id) ? new {Status = true} : new {Status = false}, JsonRequestBehavior.AllowGet);
+      }
+   }
+}
---
+++ Knockout-JS/Knockout Interop/Models/ProductRepository.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Mvc4KnockoutCRUD.Models
+{
+   public class ProductRepository : IProductRepository
+   {
+      private readonly List<Product> _products = new List<Product>();
+      private int _nextId = 1;
+
+      public ProductRepository()
+      {
+         // Add products for the Demonstration
+         Add(new Product { Name = "Computer", Category = "Electronics", Price = 23.54M });
+         Add(new Product { Name = "Laptop", Category = "Electronics", Price = 33.75M });
+         Add(new Product { Name = "iPhone4", Category = "Phone", Price = 16.99M });
+      }
+
+      public IEnumerable<Product> GetAll()
+      {
+         // TODO : Code to get the list of all the records in database
+         return _products;
+      }
+
+      public Product Get(int id)
+      {
+         // TODO : Code to find a record in database
+         return _products.Find(p => p.Id == id);
+      }
+
+      public Product Add(Product item)
+      {
+         if (item == null)
+         {
+            throw new ArgumentNullException("item");
+         }
+
+         // TODO : Code to save record into database
+         item.Id = _nextId++;
+         _products.Add(item);
+
+         return item;
+      }
+
+      public bool Update(Product item)
+      {
+         if (item == null)
+         {
+            throw new ArgumentNullException("item");
+         }
+
+         // TODO : Code to update record into database
+         var index = _products.FindIndex(p => p.Id == item.Id);
+         if (index == -1)
+         {
+            return false;
+         }
+         _products.RemoveAt(index);
+         _products.Insert(index, item);
+
+         return true;
+      }
+
+      public bool Delete(int id)
+      {
+         // TODO : Code to remove the records from database
+         _products.RemoveAll(p => p.Id == id);
+
+         return true;
+      }
+   }
+}
