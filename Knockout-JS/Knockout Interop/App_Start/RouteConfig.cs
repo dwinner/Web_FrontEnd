044e14674334fcbd0c8fd78b3433e9b7e30b4361
dwinner <dwinner@inbox.ru>
2016-08-02 12:44:19 +0300

    Adding knockout JS starting sample

---
+++ Knockout-JS/Knockout Interop/App_Start/RouteConfig.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Mvc4KnockoutCRUD
+{
+   public static class RouteConfig
+   {
+      public static void RegisterRoutes(RouteCollection routes)
+      {
+         routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+         routes.MapRoute("Default", "{controller}/{action}/{id}",
+            new { controller = "Product", action = "Product", id = UrlParameter.Optional });
+      }
+   }
+}
