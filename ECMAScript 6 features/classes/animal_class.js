c8c6e40b55a6776867a4e885f0d51d6f998366a5
SCHLEISSHEIMER\Vinevtsev <dwinner@inbox.ru>
2021-08-12 10:19:40 +0300

    classes in es6

---
+++ ECMAScript 6 features/classes/animal_class.js
@@ -0,0 +1,13 @@
+﻿export class Animal {
+    constructor(name) {
+        this.name = name;
+    }
+
+    greeting(sound) {
+        return `A ${this.name} ${sound}`;
+    }
+
+    static echo(input) {
+        console.log (input);
+    }
+}
---
+++ ECMAScript 6 features/classes/animal_consumer.js
@@ -0,0 +1,11 @@
+﻿import { Animal } from "./animal_class";
+
+export class AnimalClient {
+    constructor() {
+        this.animal = new Animal ("Dog");
+        console.log (this.animal.greeting ("barks"));
+    }
+}
+
+let animalClient = new AnimalClient();
+Animal.echo ("roof, roof");
